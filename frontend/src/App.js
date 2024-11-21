9c20b7c471aed48a0369ac2df09a34135d3e5d88
luka-danger <lukathevegan@gmail.com>
2024-11-20 17:43:57 -0700

    Update UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,53 +2,76 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { truncateDescription } from './truncate';
 import { companies } from './companyList';
+import logo from './velociraptor.gif';
+import { useAnimation } from './useAnimation'; // Import the custom animation hook
 
 function App() {
-  const [jobData, setJobData] = useState(null); 
-  const [loading, setLoading] = useState(false); 
+  const [jobData, setJobData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [companyName, setCompanyName] = useState('');
+
+  // Use the custom hook for animation
+  const typedText = useAnimation(); 
 
   // Function to fetch a random job from the backend
   const fetchRandomJob = async () => {
     setLoading(true);
     try {
-        // Get job posting from Node backend server
-        const response = await axios.get('http://localhost:5002/random_job'); 
-        setJobData(response.data);
-    } catch (error) {
-        console.error('Error fetching random job:', error);
-    } finally {
-        setLoading(false);
-    }
-};
+      // Get job posting from Node backend server
+      const response = await axios.get('http://localhost:5002/random_job');
+      setJobData(response.data);
 
+      setCompanyName(companies[Math.floor(Math.random() * companies.length)])
+
+    } catch (error) {
+      console.error('Error fetching random job:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <div>
-      <h1>Introducing...netWork <i>DinoDetect</i></h1>
-      <h2>A new way to job search</h2>
-
-      {/* Button to fetch new random job */}
-      <button onClick={fetchRandomJob} disabled={loading}>
-        {loading ? 'Loading...' : 'Next Job Posting'}
-      </button>
-
-      {/* Display job data if available */}
-      {jobData && (
-        <div>
-          <h2>Job Title: {jobData.job.title}</h2>
-          <h3>Company: {companies[Math.floor(Math.random() * companies.length)]}</h3>
-          <h4>Function: {jobData.job.function}</h4>
-          <p><b>Description:</b> {truncateDescription(jobData.job.description, 150)}</p>
-          <h4 className={`prediction ${jobData.prediction === 'Real' ? 'real' : 'fake'}`}>
-            Prediction: {jobData.prediction}
-          </h4>
+    <div className='container'>
+      <div className='upper'>
+        <div className='text'>
+          <h1 className='title'><i>Job Raptor</i></h1>
+          <h3 className='slogan'>A job posting classifier, powered by netWork</h3>
         </div>
-      )}
+      </div>
 
-      {/* Display a message when no job data is available */}
-      {!jobData && !loading && (
-        <p>Click "Next Job Posting" to run the <i>DinoDetect</i> job post classifier!</p>
-      )}
+      <div className='lower'>
+        {/* Button to fetch new random job */}
+        <button onClick={fetchRandomJob} disabled={loading}>
+          {loading ? 'Loading...' : 'Next Job Posting'}
+        </button>
+
+        {/* Display job data if available */}
+        {jobData && (
+          <div>
+            <h2>Job Title: {jobData.job.title}</h2>
+            <h3>Company: {companyName}</h3>
+            <p className='description'><b>Description:</b> {truncateDescription(jobData.job.description, 50)}</p>
+            <h4 className={`prediction ${jobData.prediction === 'Real' ? 'real' : 'fake'}`}>
+              Likely {jobData.prediction}
+            </h4>
+          </div>
+        )}
+
+        {/* Display a message when no job data is available */}
+        {!jobData && !loading && (
+          <h3>Click "Next Job Posting" to run the <i>Job Raptor</i> job post classifier!</h3>
+        )}
+      </div>
+
+      <div className='footer'>
+        <div className='logo'>
+          <img className='dino' src={logo} alt='loading...' />
+          <div className='typing'>
+            <span className='sentence'>{typedText}</span>
+            <span className='cursor'></span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
